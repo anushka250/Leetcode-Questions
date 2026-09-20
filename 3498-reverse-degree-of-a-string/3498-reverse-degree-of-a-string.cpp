1538582e11e531b590a67aa04e3d513ab92baa05
Anushka Mall <msanya0860@gmail.com>
2026-09-20 22:18:41 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n = s.length();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += ('z' - s[i] + 1) * (i+1);
+        }
+        return sum;
+    }
+};
